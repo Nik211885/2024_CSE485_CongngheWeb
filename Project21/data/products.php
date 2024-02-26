dbbf4a4ff89ae3f5b15d2c1200a1038a75945710
viet <viethbhn2003@gmail.com>
2024-02-26 12:11:26 +0700

    project21

---
+++ Project21/data/products.php
@@ -0,0 +1,32 @@
+<?php $products = [
+        [
+            "id" => 1,
+            "name" => "T-shirt",
+            "price" => 15.99,
+            "description" => "A comfortable and stylish T-shirt"
+        ],
+        [
+            "id" => 2,
+            "name" => "Jean",
+            "price" => 23,
+            "description" => "A comfortable and stylish Jean"
+        ],
+        [
+            "id" => 3,
+            "name" => "Jacket",
+            "price" => 50.99,
+            "description" => "A comfortable and stylish Jacket"
+        ],
+        [
+            "id" => 4,
+            "name" => "Short",
+            "price" => 12.99,
+            "description" => "A comfortable and stylish Short"
+        ],
+        [
+            "id" => 5,
+            "name" => "Coat",
+            "price" => 60.99,
+            "description" => "A comfortable and stylish Coat"
+        ],
+    ];
--- Project21/index.php
+++ Project21/index.php
@@ -4,52 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    
     <title>Products</title>
 </head>
 <body>
     <?php
-    $products = [
-        [
-            "id" => 1,
-            "name" => "T-shirt",
-            "price" => 15.99,
-            "description" => "A comfortable and stylish T-shirt"
-        ],
-        [
-            "id" => 2,
-            "name" => "Jean",
-            "price" => 23,
-            "description" => "A comfortable and stylish Jean"
-        ],
-        [
-            "id" => 3,
-            "name" => "Jacket",
-            "price" => 50.99,
-            "description" => "A comfortable and stylish Jacket"
-        ],
-        [
-            "id" => 4,
-            "name" => "Short",
-            "price" => 12.99,
-            "description" => "A comfortable and stylish Short"
-        ],
-        [
-            "id" => 5,
-            "name" => "Coat",
-            "price" => 60.99,
-            "description" => "A comfortable and stylish Coat"
-        ],
-    ];
-    // echo '<div class="container">';
-    // foreach ($products as $product){
-    //     echo '<div class="product">';
-    //     echo "<h2> {$product['name']} </h2>";
-    //     echo "<p> {$product['price']} </p>";
-    //     echo "<p> {$product['description']} </p>";
-    //     echo '</div>';
-    // }
-    // echo '</div>';
-
+    include "data/products.php";
+    
     $itemsPerPage = 1;
     $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
     $totalPages = ceil(count($products) / $itemsPerPage);
