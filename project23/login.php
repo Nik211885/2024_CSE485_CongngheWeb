bfdff369cddb96591760d901b55717535a19273b
tminhtri1910 <tminhtri1910@gmail.com>
2024-03-04 02:05:50 +0700

    create: pj23>login.php

---
+++ project23/login.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<?php //include "data.php"; ?>
+<form action="process_login.php" method="post">
+    Username: <input type="text" name="username" value="johndoe">
+    <br>
+    Password: <input type="password" name="password">
+    <br>
+    <button type="submit">Login</button>
+
+</form>
+</body>
+</html>
