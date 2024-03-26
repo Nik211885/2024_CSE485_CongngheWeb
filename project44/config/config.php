ab4bc07211e5bc03bad35b568f45e8730e418181
tminhtri1910 <tminhtri1910@gmail.com>
2024-03-26 20:55:48 +0700

    create: pj44>config

---
+++ project44/config/config.php
@@ -0,0 +1,12 @@
+<?php
+define("HOST", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define('DATABASE', "contacts");
+
+//define(TABLE, "department");
+$table = 'department';
+define('ROOT', dirname(__FILE__, 2));
+define('DOMAIN', 'http://localhost/2024_CSE485_CongngheWeb/project44');
+//$GLOBALS ['DOMAIN']= 'http://localhost/2024_CSE485_CongngheWeb/project44';
+
